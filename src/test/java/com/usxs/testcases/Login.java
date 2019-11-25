e45774b149d2e6137c2c69e0dacc292398ef0219
roshankallumkal <roshantom@gmail.com>
2019-11-25 11:58:14 +0530

    commit done on 25Nov2019

--- src/test/java/com/usxs/testcases/Login.java
+++ src/test/java/com/usxs/testcases/Login.java
@@ -18,6 +18,7 @@ public class Login extends TestBase {
 		
 		System.out.println("gitdemo x");
 		System.out.println("gitdemo xX");
+		System.out.println("update from gitdemo xxX");
 		
 		//Assert Title USXS 2.0 
 		Assert.assertTrue(IsElementPresent(By.xpath(Obj_repo.getProperty("pagetitle"))),"Login page is not loaded");

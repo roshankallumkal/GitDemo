b81debb6fd91493630328b517d37c7b79d3e4531
roshankallumkal <roshantom@gmail.com>
2019-11-25 12:43:34 +0530

    architect changes 25Nov2019

--- src/test/java/com/usxs/testcases/Login.java
+++ src/test/java/com/usxs/testcases/Login.java
@@ -16,28 +16,19 @@ public class Login extends TestBase {
 	public void login_as_super_admin(String username,String password) throws InterruptedException{
 		
 		
-		System.out.println("gitdemo x");
-		System.out.println("gitdemo xX");
-		System.out.println("update from gitdemo xxX");
-		
+		System.out.println("check subbranch");		
 		//Assert Title USXS 2.0 
-		Assert.assertTrue(IsElementPresent(By.xpath(Obj_repo.getProperty("pagetitle"))),"Login page is not loaded");
-		log.debug("Inside log in module");
-		//Find and provide value for 'Email' field
-		driver.findElement(By.xpath(Obj_repo.getProperty("emailBtn"))).sendKeys(username);
-		//Find and provide value for 'Password' field
-		driver.findElement(By.xpath(Obj_repo.getProperty("pwdBtn"))).sendKeys(password);
-		//Find and click 'Login button'
-		driver.findElement(By.xpath(Obj_repo.getProperty("loginBtn"))).click();
-		Assert.assertTrue(IsElementPresent(By.xpath(Obj_repo.getProperty("userImg"))),"Home page is not loaded");
-		//this.ClickHiddenElement(By.xpath("//*[@id=\"wrapper\"]/nav/ul[3]/li[3]/a/i"));
-	   	Reporter.log("Logged  in successfully");
+		
 	   	//Reporter.log("<a href=\"E:\\SeleniumSS\\error.jpg\">Screenshot</a>");
 	    
 		//Thread.sleep(3000);
 	   //	
 	}
 	
+	@Test
+	public void subbranchsetup() {
+		System.out.println("Code added for sub branch cases");
+	}
 	
 
 }
